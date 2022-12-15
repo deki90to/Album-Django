2962b6dd73b85ade0de4166bfd1b95423ec0b49b
deki90to <deki90to@gmail.com>
2022-12-15 02:13:37 +0100

    user-dislikes-profile-views

--- baseApp/views.py
+++ baseApp/views.py
@@ -281,7 +281,7 @@ def addDislike(request, pk):
 
 def display_all_likes(request, pk):
     album_details = Album.objects.get(pk=pk)
-    album_likes = album_details.likes.all().order_by('-likes')
+    album_likes = album_details.likes.all()
     return render(request, 'baseApp/parts/display_all_likes.html', {
         'album_likes': album_likes
     })
