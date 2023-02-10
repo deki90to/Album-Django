51f990a29c48cc9e85f8e97f82ccc3f3df80b09a
dejan <deki90to@gmail.com>
2023-02-11 00:31:46 +0100

    fixed linking from likes and commenting

--- AlbumDjangoHtmxProject/settings.py
+++ AlbumDjangoHtmxProject/settings.py
@@ -144,5 +144,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'deki90to@gmail.com'
-EMAIL_HOST_PASSWORD = '' #Your email account password
+EMAIL_HOST_PASSWORD = 'ovvdafnqjcrlrzva' #Your email account password
 
